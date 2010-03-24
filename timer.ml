eb8dddef96e10e0eeb9ed739a0ab45779a16d365
Ranjit Jhala <jhala@chowder.ucsd.edu>
2010-03-23 21:57:51 -0700

    twiddles

--- timer.ml
+++ timer.ml
@@ -58,13 +58,15 @@ let print ppf t =
 (*
 let rec pause n = if n > 0 then pause (n-1) 
 
-let rec sim n b t = 
+let rec sim n c b t = 
   if n > 0 then begin
     log_event t (Some ("downtick "^(string_of_int n))); 
-    pause b; sim (n-1) (2*b) t
+    pause b; sim (n-1) c (c*b) t
   end
 
+let c = try Sys.argv.(1) |> int_of_string with _ -> 2
 let _ = create "boo" 
-        >> sim 8 9999999 
+        >> sim 8 c 9999999 
         |> Format.printf "%a" print 
+
 *)
