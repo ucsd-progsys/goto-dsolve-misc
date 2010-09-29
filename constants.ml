fb013aa4934eeac9609dab273bf4913a6566d3a1
rybal <rybal@noname.(none)>
2010-09-29 16:40:36 +0200

    qweqwe

--- constants.ml
+++ constants.ml
@@ -38,6 +38,7 @@ let verbose_level       = ref ol_default       (* -v *)
 let latex_file: string option ref = ref None   (* translate to LaTeX *)
 let armc_file: string option ref  = ref None   (* translate to ARMC *)
 let horn_file: string option ref  = ref None   (* translate to Horn clauses *)
+let raw_horn_file: string option ref  = ref None   (* translate to raw Horn clauses *)
 let q_armc_file: string option ref = ref None   (* OBSOLETE translate to Q'ARMC file *)
 let dot_file: string option ref = ref None   (* translate to dot file *)
 let purify_function_application = ref true  (* replace fun-terms by existentially quantified variables *)
@@ -189,6 +190,15 @@ let arg_spec =
 		    else
 		      horn_file := Some s),
     " translate constraints to Horn clauses"
+   );
+   ("-raw-horn", 
+    Arg.String (fun s -> 
+		  let l = String.length s in
+		    if l = 0 then
+		      print_endline "-rules: invalid parameter"
+		    else
+		      raw_horn_file := Some s),
+    " translate constraints to raw Horn clauses"
    );
    ("-qarmc", 
     Arg.String (fun s -> 
